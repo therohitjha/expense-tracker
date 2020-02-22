97ccc60a96ac40f57632d87f95890acd6aa88c61
Rohit Jha <45750703+therohitjha@users.noreply.github.com>
2020-02-22 23:00:21 +0530

    Updates

---
+++ precache-manifest.61002bd74374225f84c476de730ad08e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7856a4729e99bccd36e058fdf319e3c",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "cca2f7cf990bc3e2d158",
+    "url": "/expense-tracker/static/css/main.6ebd63e0.chunk.css"
+  },
+  {
+    "revision": "1d451f595efbb43016ab",
+    "url": "/expense-tracker/static/js/2.6ebd275a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/expense-tracker/static/js/2.6ebd275a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cca2f7cf990bc3e2d158",
+    "url": "/expense-tracker/static/js/main.a01bf6db.chunk.js"
+  },
+  {
+    "revision": "0f5aa16d734b9726422c",
+    "url": "/expense-tracker/static/js/runtime-main.61d7beed.js"
+  }
+]);
